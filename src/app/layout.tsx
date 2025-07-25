a4520f57b3ef00607cff359c931dcefb0f8e986e
Sarwar Hossain <sarwarhridoy4@gmail.com>
2025-07-25 09:12:05 +0600

    feat: setup initial layout with a navbar and footer components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
-        {children}
+        <NavBar />
+
+        <main className='flex-1 flex items-center justify-center'>
+          {children}
+        </main>
+
+        <Footer />
       </body>
     </html>
   );
